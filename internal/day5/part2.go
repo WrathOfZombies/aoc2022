8215d4be91e5693166f3c28f7e45016f92253903
Bhargav Krishna <3244360+WrathOfZombies@users.noreply.github.com>
2022-12-05 08:38:58 +0000

    chore(bootstrap): bootstrapping day 5 and adding template

---
+++ internal/day5/part2.go
@@ -0,0 +1,17 @@
+package day5
+
+import (
+	"fmt"
+
+	stringUtils "github.com/wrathofzombies/aoc2022/internal/common/string_utils"
+)
+
+// https://adventofcode.com/2022/day/5
+// Given an input find the number of ranges with full overlaps
+func SolvePart2(input string) int {
+	totalScore := 0
+	for line := range stringUtils.GetLine(input) {
+		fmt.Printf("line: %s\n", line)
+	}
+	return totalScore
+}
